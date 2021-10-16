948185c803c821e5dc187fb2dc62326b7d264778
arnavpuranik <puranikarnav@gmail.com>
2021-10-16 09:08:40 +0000

    whyred: Add Google Camera Go Prebuilt * On Camera2 there's an issue with Video Recording where the Camera app Crashes while we start recording
    Signed-off-by: arnavpuranik <puranikarnav@gmail.com>

---
+++ prebuilt/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH := $(call my-dir)
+
+# GCamGo
+include $(CLEAR_VARS)
+LOCAL_MODULE := GoogleCameraGo
+LOCAL_SRC_FILES := GoogleCameraGo/GoogleCameraGo.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := Snap Camera2
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
